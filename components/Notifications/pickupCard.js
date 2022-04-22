b20f44bf037ae6727cae2136b032da6c4b9c470d
Hassananwar12030 <53085534+Hassananwar12030@users.noreply.github.com>
2022-04-22 06:47:32 +0500

    adding open location in maps for pickup

--- components/Notifications/pickupCard.js
+++ components/Notifications/pickupCard.js
@@ -1,11 +1,23 @@
 import React, {useContext} from 'react';
-import { View, TouchableOpacity, Text } from 'react-native';
+import { View, TouchableOpacity, Text, Linking } from 'react-native';
 import styles from './styles';
 import {SocketContext} from "../../context/socket";
 
 const PickupCard = ({pickup, onClickPickup, onClickReject, reject}) => {
-
+    
     const socket = useContext(SocketContext);
+    const openMaps = () =>{
+        if(pickup.pickupCoordinate){
+            const scheme = Platform.OS === 'ios' ? 'maps:0,0?q=' : 'geo:0,0?q=';
+            const latLng = `${pickup.pickupCoordinate.coordinates[1]},${pickup.pickupCoordinate.coordinates[0]}`;
+            const label = 'Custom Label';
+            const url = Platform.OS === 'ios' ? `${scheme}${label}@${latLng}` : `${scheme}${latLng}(${label})`
+            Linking.openURL(url);
+        }
+        else{
+            alert("No Location Assigned")
+        }
+    }
     const rejectButton = ()=>{
         if(!reject){
             return(
@@ -17,13 +29,14 @@ const PickupCard = ({pickup, onClickPickup, onClickReject, reject}) => {
     }
     return (
         <View style={styles.modalView} key={pickup._id}>
-
             <Text style={styles.requestHeader}>Pickup Loaction:</Text>
             <Text style={styles.detailsText}>{pickup.pickupAddress}</Text>
-
+            <TouchableOpacity onPress={openMaps} style={styles.mapsButton}>
+                <Text style={styles.mapsButtonText}>Open in Maps</Text>
+            </TouchableOpacity>
             <Text style={[styles.requestHeader, { marginTop: '3%' }]}>Dropoff Loaction:</Text>
             <Text style={styles.detailsText}>{pickup.deliveryAddress}</Text>
-
+            
             <Text style={[styles.requestHeader, { marginTop: '3%' }]}>Food Details:</Text>
             <Text style={styles.detailsText}>{pickup.description}</Text>
 
